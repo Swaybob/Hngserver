a3c7dbca8f59a13c4018eaa51ea3fba6ea847962
Marvellous <adeoyemarvellous7@gmail.com>
2022-10-30 00:45:56 +0100

    initial commit

--- hng1/settings.py
+++ hng1/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=^rg^34_dfn=f1f--k5zko275g6899-jh9u(_kr&%76hla)6w7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'vectorserver.herokuapp.com']
 
 
 # Application definition
